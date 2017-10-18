f330cbf1c0d528fcff87d6a5f8532c446a86bc38
Zach Berman <zberms@gmail.com>
2017-10-18 08:42:56 -0700

    Initial Commit/First Draft

---
+++ js/active.js
@@ -0,0 +1,183 @@
+(function ($) {
+    'use strict';
+
+    // collapse mobile menu
+    $(document).on('click','.navbar-collapse',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+    });
+
+    // Gallery Menu Style Active Code
+    $('.portfolio-menu button.btn').on('click', function () {
+        $('.portfolio-menu button.btn').removeClass('active');
+        $(this).addClass('active');
+    })
+
+   // SmoothScroll
+    $('a[href*="#"]')
+   // Remove links that don't actually link to anything
+  .not('[href="#"]')
+   // Remove links action not required
+  .not('[href="#0"]')
+  .not('[href="#nav"]')
+  .not('[href="#carouselOne"]')
+  .not('[href="#carouselTwo"]')
+  .not('[href="#carouselThree"]')
+  .not('[href="#carouselFour"]')
+  .not('[href="#carouselFive"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+    // Magnific-popup Video Active Code
+    if ($.fn.magnificPopup) {
+        $('.video_btn').magnificPopup({
+            disableOn: 0,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: true,
+            fixedContentPos: false
+        });
+        $('.gallery_img.yellowstone-springs').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        $('.gallery_img.bigsprings').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        $('.gallery_img.fintail').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        $('.gallery_img.tarryall').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        $('.gallery_img.riverwhy').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        $('.gallery_img.steelcreek').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        $('.gallery_img.summit').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        $('.gallery_img.madison').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        $('.gallery_img.jefferson').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        $('.gallery_img.clover').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        $('.gallery_img.littlewood').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+    }
+
+    // ScrollUp Active Code
+    if ($.fn.scrollUp) {
+        $.scrollUp({
+            scrollSpeed: 1000,
+            easingType: 'easeOutSine',
+            scrollText: '<i class="fa fa-long-arrow-up" aria-hidden="true"></i>'
+        });
+    }
+
+    // Masonary Gallery Active Code
+    $('.gallery_full_width_images_area, .portfolio-column, .blog-masonary').imagesLoaded(function () {
+        // filter items on button click
+        $('.portfolio-menu').on('click', 'button', function () {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({
+                filter: filterValue
+            });
+        });
+        // init Isotope
+        var $grid = $('.gallery_full_width_images_area, .portfolio-column, .blog-masonary').isotope({
+            itemSelector: '.single_gallery_item, .column_single_gallery_item, .sb_masonary_item',
+            percentPosition: true,
+            filter: '.bigsprings',
+            masonry: {
+                columnWidth: '.single_gallery_item, .column_single_gallery_item, .sb_masonary_item'
+            }
+        });
+    });
+
+    // Parallax active js
+    if ($.fn.jarallax) {
+        $('.jarallax').jarallax({
+            speed: 0.2
+        });
+    }
+
+    // :: 8.0 MatchHeight Active Code
+    if ($.fn.matchHeight) {
+        $('.item').matchHeight();
+    }
+
+    var $window = $(window);
+
+
+})(jQuery);
